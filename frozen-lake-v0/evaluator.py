59e172be9443c13b3717c6f2cb1c4f791569a088
Javier V <javier.vargas@screeningeagle.com>
2020-05-09 11:40:36 +0000

    Added frozenlake evaluator

---
+++ frozen-lake-v0/evaluator.py
@@ -0,0 +1,78 @@
+import os
+import time
+import sys
+
+import argparse
+import gym
+import torch as T
+from tqdm import tqdm
+
+sys.path.append('.')
+from rl.utils import get_logger
+from rl.metrics import Recorder
+from rl.weights import format_weights_name, load_weights
+from rl.agents.qlearning import SARSAAgent
+
+
+def render(environment, n, reward, time_delta=0.2):
+    """Helps the rendering in terminal
+    """
+    # Clearing terminal
+    os.system('clear')
+
+    # Logging & rendering
+    print('Iteration: {} Step reward: {:.2f}'.format(n, reward))
+    environment.render()
+
+    # Sleeping for a while to see evolution
+    time.sleep(time_delta)
+
+
+def play(weights):
+    """Evaluate an agent playing Taxi-v3
+    """
+    log = get_logger(__name__)
+    
+    # Building environment & reseting to init state
+    log.info('Building environment')
+    env = gym.make('FrozenLake-v0')
+    state = env.reset()
+
+    # Creating the agent
+    log.info('Creating agent')
+    N_states = env.observation_space.n
+    N_actions = env.action_space.n
+    agent = SARSAAgent(state, N_states, N_actions)
+
+    # Loading agent weights
+    log.info('Loading agent weights: %s', weights)
+    load_weights(agent, weights)
+
+    # Reseting environment for new episode
+    state = env.reset()
+    ended = False
+    iteration = 0 
+
+    while not ended:
+        # Playing
+        action = agent(state)
+        state, reward, ended, info = env.step(action)
+        agent.update(state, reward)
+
+        # Rendering
+        render(env, iteration, reward, time_delta=0.2)
+
+        # Updating iterations
+        iteration += 1
+
+
+if __name__== '__main__':
+    # Parse args
+    description = 'FrozenLake-v0 agent evaluator'
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('weights', 
+                        help='Path to weigths to load for the agent')
+    args = parser.parse_args()
+
+    # Playing
+    play(weights=args.weights)
